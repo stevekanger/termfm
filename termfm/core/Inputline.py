be640b94c22cee2831cfeb2350445e6ed5a0e596
stevekanger <hi.stevekanger@gmail.com>
2025-02-10 00:58:55 -0600

    Changed to 0 index cursor in inputline

--- termfm/core/Inputline.py
+++ termfm/core/Inputline.py
@@ -12,7 +12,7 @@ class Inputline:
         self.id: Literal["inputline"] = "inputline"
         self.app: App = app
         self.input: List[str] = []
-        self.cursor_pos: int = 1
+        self.cursor_pos: int = 0
         self.win: curses.window = create_curses_win(app.stdscr, self.id)
 
     def resize(self):
@@ -36,17 +36,18 @@ class Inputline:
                 start_range = input_len - max_width
 
             self.win.addstr(0, 0, ":")
+
             for i in range(start_range, input_len):
-                if i + 1 == self.cursor_pos:
+                if i == self.cursor_pos:
                     self.win.addstr(
                         0, i - start_range + 1, self.input[i], color_pair("cursor")
                     )
                 else:
                     self.win.addstr(0, i - start_range + 1, self.input[i])
 
-            if self.cursor_pos > input_len:
+            if self.cursor_pos == input_len:
                 self.win.addstr(
-                    0, input_len - start_range + 1, " ", color_pair("cursor")
+                    0, self.cursor_pos - start_range + 1, " ", color_pair("cursor")
                 )
 
         self.win.refresh()
@@ -54,26 +55,25 @@ class Inputline:
     def add_char(self, key: str):
         if not key:
             return
-        self.input.insert(self.cursor_pos - 1, key)
+        self.input.insert(self.cursor_pos, key)
         self.increase_cursor_pos()
 
     def remove_char(self):
-        input_len = len(self.input)
-        if input_len <= 0:
+        if self.cursor_pos == 0 and not len(self.input):
             self.clear()
-        else:
-            del self.input[self.cursor_pos - 2]
+        elif self.cursor_pos > 0:
             self.decrease_cursor_pos()
+            del self.input[self.cursor_pos]
 
     def increase_cursor_pos(self):
-        self.cursor_pos = min(len(self.input) + 1, self.cursor_pos + 1)
+        self.cursor_pos = min(len(self.input), self.cursor_pos + 1)
 
     def decrease_cursor_pos(self):
-        self.cursor_pos = max(1, self.cursor_pos - 1)
+        self.cursor_pos = max(0, self.cursor_pos - 1)
 
     def clear(self):
         self.input = []
-        self.cursor_pos = 1
+        self.cursor_pos = 0
         self.app.switch_mode("panel")
 
     def get_input(self):
